af67f434190076b45c2f8f258b1d12a109f605d3
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-02 17:54:42 -0500

    Create ticketbuildscript.js

---
+++ js/ticketbuildscript.js
@@ -0,0 +1,20 @@
+function submitdata(){
+    let cdate =Date().toTimeString()
+    let data={
+        scale:"",
+        urgency:"",
+        prior:"",
+        resolv:"",
+        approv:0,
+        hasimg:0,
+        submit:"",
+        inprg:0,
+        descr:"",
+        loc:"",
+        name:""
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST","192.168.10.4"
+        
+    )
+};
