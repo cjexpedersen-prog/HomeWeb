0318b7976ac6053d83f7c770880390297b406708
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-03 15:32:14 -0500

    Update ticketbuildscript.js

--- js/ticketbuildscript.js
+++ js/ticketbuildscript.js
@@ -1,6 +1,9 @@
-function submitdata(){
-    let cdate =Date().toTimeString()
-    let data={
+const form = document.querySelector("form")
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        submitdata();
+    })
+let data={
         scale:"",
         urgency:"",
         prior:"",
@@ -13,11 +16,19 @@ function submitdata(){
         loc:"",
         name:""
     }
+function submitdata(){
+    const formData = new formData(form)
+    data.descr =document.getElementById("ibDescr").innerText
+    data.loc =document.getElementById("ibLocat").innerText
+    data.submit =Date().toTimeString()
+    data.scale= formData.get("uR")
+    data.urgency= formData.get("sR")
+    data.prior= formData.get("existR")
     let xhr = new XMLHttpRequest();
     let url = "../script/sqlcatch.php"
+
     xhr.open("POST",url, true)
     xhr.setRequestHeader("Content-Type","application/json");
-
     xhr.onreadystatechange = function(){
         if(xhr.readystate === 4 && xhr.status === 200){
             document.getElementById("tester").innerText=this.responseText;
