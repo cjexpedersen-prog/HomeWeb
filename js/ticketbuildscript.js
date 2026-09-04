0ac0f3d716e42c1dea8067a80922e0bb56930ff7
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-03 19:55:57 -0500

    Update ticketbuildscript.js

--- js/ticketbuildscript.js
+++ js/ticketbuildscript.js
@@ -64,7 +64,7 @@ function submitdata(){
 async function trynew(){
         try{
                 let response = await fetch("../test.txt");
-                let rdata = await response.json();
+                let rdata = await response;
                 console.log(rdata);
         }
         catch (error){
