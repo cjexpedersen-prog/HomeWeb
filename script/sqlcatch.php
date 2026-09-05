2cd1c016bb15fbbee5395a378d53f2310fe3e6d0
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-05 17:00:09 -0500

    Update sqlcatch.php

--- script/sqlcatch.php
+++ script/sqlcatch.php
@@ -1,8 +1,4 @@
 <?php
-
-header("Content-Type: application/json");
-
 $data = json_decode(file_get_contents("php://input"));
-
-echo "Received ticket of $urgency importance and $scale size."
+echo "Received ticket of $data->importance and $data->scale size."
 ?>
