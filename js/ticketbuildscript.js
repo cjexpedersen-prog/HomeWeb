141a04ba84b42850bd4d715c5c52989b1abaee9c
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-05 19:58:03 -0500

    Update ticketbuildscript.js

--- js/ticketbuildscript.js
+++ js/ticketbuildscript.js
@@ -63,8 +63,8 @@ function submitdata(){
     };
     xhr.open("POST",url, true)
         xhr.setRequestHeader("Content-Type","application/json;charset=UTF-8");
-        xhr.send(JSON.stringify(data));
-        console.log(xhr.response);
+        xhr.send(data);
+        console.log(xhr.responseText);
 };
 async function trynew(){
     try{
