9f58824a22065ff002b4b819d58fee398eeae31a
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-07 13:01:05 -0500

    Update sqlcatch.php

--- script/sqlcatch.php
+++ script/sqlcatch.php
@@ -5,7 +5,7 @@ $pass = file_get_contents('passwd.txt');
 $dbn = file_get_contents('dbn.txt');
 $sqll = mysqli_connect('localhost',$user,$pass,$dbn);
 if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " , mysqli_connect_error(), <br>, $user, $pass, $dbn, $sqll;
+  echo "Failed to connect to MySQL: " , mysqli_connect_error(), "<br>", $user, $pass, $dbn, $sqll;
   exit();
 }
 else{
