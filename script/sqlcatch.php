c0e30133e01f9f2d2918926317fb2991ce96eaed
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-07 12:57:38 -0500

    Update sqlcatch.php

--- script/sqlcatch.php
+++ script/sqlcatch.php
@@ -1,5 +1,17 @@
 <?php
 $data = json_decode( file_get_contents( 'php://input' ), true );
+$user = file_get_contents('user.txt');
+$pass = file_get_contents('passwd.txt');
+$dbn = file_get_contents('dbn.txt');
+$sqll = mysqli_connect('localhost',$user,$pass,$dbn);
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+else{
+$stmt = $mysqli->prepare("INSERT INTO hhouse (scale,urgency,prior,hasimage,submittedwhen,description,location) VALUES(?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param($data['scale'], $data['urgency'], $data['prior'], $data['hasimg'], $data['submit'], $data['descr'], $data['loc'];
+$stmt->execute();
 echo "ticket recieved at ",$data['submit'];
-
+}
 ?>
