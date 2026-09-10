d8f1d907bc9b8bd698a17183437ca3a376872ba9
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-09 22:32:34 -0500

    Update sqlcatch.php

--- script/sqlcatch.php
+++ script/sqlcatch.php
@@ -1,19 +1,24 @@
 <?php
-try{
+
 $data = json_decode( file_get_contents( 'php://input' ), true );
-try{
- include_once("sqlconnect.php");
-  $stmt = $mysqli->prepare("INSERT INTO hhouse (scale,urgency,prior,hasimage,submittedwhen,description,location) VALUES(?, ?, ?, ?, ?, ?, ?)");
-  $stmt->bind_param($data['scale'], $data['urgency'], $data['prior'], $data['hasimg'], $data['submit'], $data['descr'], $data['loc']);
-  $stmt->execute();
-  echo "ticket recieved at ", $data['submit'];
-  exit();
-}
-catch(Exception $e){
-  echo 'Message: ' .$e->getMessage();
-}
-}
-catch(Exception $e){
-  echo 'Message: ' .$e->getMessage();
-}
+
+include_once("sqlconnect.php");
+ $stmt = $mysqli->prepare("INSERT INTO hhouse (scale, urgency, prior, hasimage, submittedwhen, description, location) VALUES(:scale, :urgency, :prior, :hasimage, :submittedwhen, :description, :location)");
+ $stmt->bind_param(':scale', $sc)
+ $stmt->bind_param(':urgency', $urg)
+ $stmt->bind_param(':prior', $pri)
+ $stmt->bind_param(':hasimage', $img)
+ $stmt->bind_param(':submittedwhen', $when)
+ $stmt->bind_param(':description', $descr)
+ $stmt->bind_param(':location', $loc)
+ $sc=$data['scale']; 
+ $ur=$data['urgency'];
+ $pri=$data['prior'];
+ $img=$data['hasimg'];
+ $when=$data['submit'];
+ $descr=$data['descr'];
+ $loc=$data['loc']);
+ $stmt->execute();
+ echo "ticket recieved at ", $data['submit'];
+ exit();
 ?>
