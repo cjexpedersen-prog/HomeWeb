756ea3b30d8aa8f9527b8fa3660e608b38fb8d39
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-09 22:59:17 -0500

    Update sqlconnect.php

--- script/sqlconnect.php
+++ script/sqlconnect.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = "mysql:host=<127.0.0.1>;dbname=tickets;charset=utf8mb4";
+$dsn = "mysql:host=localhost; dbname=tickets;"
 
 $options = [
   PDO::ATTR_EMULATE_PREPARES   => false, // Disable emulation mode for "real" prepared statements
