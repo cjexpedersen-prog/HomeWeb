e6c846b8c2fdbfbebde8d0a4ca8c2dc2d2a19788
cjexpedersen-prog <cjexpedersen@gmail.com>
2026-09-09 22:36:22 -0500

    Update sqlcatch.php

--- script/sqlcatch.php
+++ script/sqlcatch.php
@@ -4,13 +4,13 @@ $data = json_decode( file_get_contents( 'php://input' ), true );
 
 include_once("sqlconnect.php");
  $stmt = $mysqli->prepare("INSERT INTO hhouse (scale, urgency, prior, hasimage, submittedwhen, description, location) VALUES(:scale, :urgency, :prior, :hasimage, :submittedwhen, :description, :location)");
- $stmt->bind_param(':scale', $sc)
- $stmt->bind_param(':urgency', $urg)
- $stmt->bind_param(':prior', $pri)
- $stmt->bind_param(':hasimage', $img)
- $stmt->bind_param(':submittedwhen', $when)
- $stmt->bind_param(':description', $descr)
- $stmt->bind_param(':location', $loc)
+ $stmt->bind_param(':scale', $sc);
+ $stmt->bind_param(':urgency', $urg);
+ $stmt->bind_param(':prior', $pri);
+ $stmt->bind_param(':hasimage', $img);
+ $stmt->bind_param(':submittedwhen', $when);
+ $stmt->bind_param(':description', $descr);
+ $stmt->bind_param(':location', $loc);
  $sc=$data['scale']; 
  $ur=$data['urgency'];
  $pri=$data['prior'];
